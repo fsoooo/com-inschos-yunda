d25bc147d9f92081162a5dc9609872fa70ff8517
wangshilei <wangsl@inschos.com>
2018-06-25 16:28:32 +0800

    '06.25-外接接口'

--- src/main/java/com/inschos/yunda/model/WarrantyRecord.java
+++ src/main/java/com/inschos/yunda/model/WarrantyRecord.java
@@ -2,9 +2,15 @@ package com.inschos.yunda.model;
 
 public class WarrantyRecord {
     public long cust_id;//用户id
+    //保单信息
     public String warranty_uuid;//'warranty_uuid内部保单唯一标识'
     public String warranty_status;//保单状态 1待处理, 2待支付,3待生效, 4保障中,5可续保，6已失效，7已退保  8已过保',
     public String warranty_status_text;//保单状态文案 1待处理, 2待支付,3待生效, 4保障中,5可续保，6已失效，7已退保  8已过保',
+    //支付信息
+    public String pay_no;
+    public String pay_type;
+    public String pay_url;
+    //时间
     public long created_at;
     public long updated_at;
     //查询使用
